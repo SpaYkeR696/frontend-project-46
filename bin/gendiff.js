4e8c21a0d0f9c6032b06137f5f5603c055119975
SpaYkeR696 <spayker666999@gmail.com>
2023-07-31 19:15:12 +0300

    fix bags

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,7 +1,8 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
-import process from 'process';
+// eslint-disable-next-line import/no-unresolved
+import process from 'commander-process';
 import genDiff from '../index.js';
 
 const program = new Command();
